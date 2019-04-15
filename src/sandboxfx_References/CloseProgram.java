176195482c8d0a316a464dad74a348e42c142843
Tran <tran.pham@hotmail.com>
2019-04-15 00:36:49 -0400

    Update CloseProgram.java
    ~Modified: Add comments to claim that these codes are NOT mine. They are here just for reference.

--- src/sandboxfx_References/CloseProgram.java
+++ src/sandboxfx_References/CloseProgram.java
@@ -9,6 +9,16 @@ import javafx.stage.Stage;
 
 public class CloseProgram extends Application{
 	
+	/*
+
+	 * The code in this class is from https://www.youtube.com/watch?v=ZuHcl5MmRck&t=107s
+	 *
+	 * This code is "vital" to the functionality of the game (so that it has 1 close button to properly close the game), but since it is not
+	 * used to fulfill any of the assignment requirements we request it be ignored for academic purposes as we take
+	 * no credit for it and we will simply use it as a pre-written library by a 3rd party.
+
+	 */
+	
 	 public static void main(String[] args) {
 	        launch(args);
 	    }
