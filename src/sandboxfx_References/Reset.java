6b86838778dd46d0cc299efc5fba4c93a75c3b5c
Tran <tran.pham@hotmail.com>
2019-04-15 00:39:38 -0400

    Update Reset.java
    ~Modified: Add comments to claim that these codes are NOT mine. They are here just for reference.

--- src/sandboxfx_References/Reset.java
+++ src/sandboxfx_References/Reset.java
@@ -13,7 +13,15 @@ import javafx.stage.Stage;
  
 public class Reset extends Application {
  
-	// https://pastebin.com/CjEqRQcc
+        /*
+
+	 * The code in this class is from https://pastebin.com/CjEqRQcc
+	 *
+	 * This code is "vital" to the functionality of the game (so that the game has one reset/restart button), but since it is not
+	 * used to fulfill any of the assignment requirements we request it be ignored for academic purposes as we take
+	 * no credit for it and we will simply use it as a pre-written library by a 3rd party.
+
+	 */
 	
     @Override
     public void start(Stage primaryStage) {
