8244b9833c58b1ec9b7baa6ed65ae98c879f4e49
Tran <tran.pham@hotmail.com>
2019-04-15 00:35:30 -0400

    Update BackgroundImage.java
    ~Add comments to claim that these codes are NOT mine. They are here just for reference.

--- src/sandboxfx_References/BackgroundImage.java
+++ src/sandboxfx_References/BackgroundImage.java
@@ -9,6 +9,16 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class BackgroundImage extends Application {
+	
+	/*
+
+	 * The code in this class is from https://www.youtube.com/watch?v=F8bY18QAj6U
+	 *
+	 * This code is "vital" to the functionality of the game (so that the game has a background), but since it is not
+	 * used to fulfill any of the assignment requirements we request it be ignored for academic purposes as we take
+	 * no credit for it and we will simply use it as a pre-written library by a 3rd party.
+
+	 */
 
 	public void start(Stage stage) {
 		
