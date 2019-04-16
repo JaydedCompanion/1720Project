a02d7bd3694dbae789bea4a6fb4b7bdf8b9269c5
Tran <tran.pham@hotmail.com>
2019-04-15 22:21:38 -0400

    Update SlidingViewTest.java

--- src/Game/SlidingViewTest.java
+++ src/Game/SlidingViewTest.java
@@ -25,18 +25,25 @@ import javafx.util.Duration;
 
 public class SlidingViewTest extends Application {
 	
+	/*
+
+	 * Some of the code (specifically the slide show) in this class is from https://stackoverflow.com/questions/27089627/javafx-switch-scene-with-slide-effect
+	 *
+	 * This code is "vital" to the functionality of the game (so that the game has slide show effect), but since it is not
+	 * used to fulfill any of the assignment requirements we request it be ignored for academic purposes as we take
+	 * no credit for it and we will simply use it as a pre-written library by a 3rd party.
+
+	 */
+	
 	 public static void main(String[] args) {
 	        launch(args);
 	    }
 	 
-	//TUTORIAL: https://stackoverflow.com/questions/27089627/javafx-switch-scene-with-slide-effect 
-	private int WIDTH = 800;
-	private int HEIGHT = 600;
+	private int WIDTH = 1000;
+	private int HEIGHT = 200;
 	private Stage primaryScene;
 	private Scene scene1, scene2;
 	Stage window;
-	char gate1 = 'c';
-	int gate2 = 1;
 
     @Override
     public void start(Stage primaryStage) {
@@ -45,26 +52,27 @@ public class SlidingViewTest extends Application {
     	GridPane grid = new GridPane();
 		StackPane stack = new StackPane ();
 		
-    	////////////////////SCENE 1/////////////////////////
 		primaryStage.setTitle("Game Project");
+		
+    	////////////////////SCENE 1/////////////////////////
 		Button button1 = new Button("Click this button for tutorial.");
-		grid.add(button1, 10, 10);
-		button1.setOnAction(e -> window.setScene(scene2));	
-		grid.setAlignment(Pos.TOP_LEFT);
+		button1.setOnAction(e -> window.setScene(scene2));
+		grid.setAlignment(Pos.BOTTOM_RIGHT);
+		grid.add(button1, 0, 0);
 		
 		Label label = new Label("Welcome! We need your help!");
 		label.setFont(Font.font("Times New Roman", FontWeight.BOLD, 30));
 		grid.add(label, 0, 2);
 		
-		Text textFont = new Text("MOVE THE SHAPES");
+		Text textFont = new Text("GUIDE THE SHAPES INTO THE CORRECT HOLE!");
 		textFont.setFont(Font.font("Verdana", FontWeight.BOLD, 30));
 		textFont.setFill(Color.CORAL);
-		grid.add(textFont, 4, 9);
+		grid.add(textFont, 2, 3);
 		
 		Text developersText = new Text("Developers: Juan, Vlad, Tran");
 		developersText.setFont(Font.font("Arial", FontWeight.BOLD, 30));
 		developersText.setFill(Color.RED);
-		grid.add(developersText, 2, 3);
+		grid.add(developersText, 4, 9);
 		
     	Image angry = new Image("Res/angry.png");
 		ImageView imgNode2 = new ImageView(angry);
@@ -73,57 +81,69 @@ public class SlidingViewTest extends Application {
 		grid.add(imgNode2, 0, 3);
 		
 		//Background image
-		Image umbrella = new Image("Res/backgroundImage.png");
-		double widthBackground = umbrella.getWidth();
-		double heightBackground = umbrella.getHeight();
 		StackPane stackBackground = new StackPane ();
+		Image background = new Image("Res/backgroundImage.png");
+		double widthBackground = background.getWidth();
+		double heightBackground = background.getHeight();
 		Scene sceneBackground = new Scene (stackBackground, widthBackground, heightBackground);
-
 		try {
 			ImageView backgroundView = new ImageView ();
-			stackBackground.getChildren().add(backgroundView);
-			
-			primaryStage.setScene(sceneBackground);
-			primaryStage.show();
+			backgroundView.setImage(background);
+			//stackBackground.getChildren().add(backgroundView);	
+			//primaryStage.setScene(sceneBackground);
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
-		//stack.getChildren().addAll(stackBackground, label, textFont,imgNode2, developersText, button1);
 		scene1 = new Scene(stack, WIDTH, HEIGHT);
 		
         //Button for next tutorial
         Button nextView = new Button("Click this button for tutorial.");
         nextView.setPadding(new Insets(5));
-        BorderPane view1 = new BorderPane(imgNode2, developersText, stackBackground, nextView, textFont);
+        BorderPane view1 = new BorderPane(stackBackground, textFont, imgNode2, nextView, developersText);
         BorderPane.setAlignment(nextView, Pos.TOP_LEFT);
-        
-		button1.setOnAction(e -> window.setScene(scene2));	
 
-    	////////////////////SCENE 2/////////////////////////
-        Button button2 = new Button("Click the box to play game!");
-        button2.setOnAction(e -> window.setScene(scene1));
-        
+    	////////////////////SCENE 2/////////////////////////   
 		Text tutorialText = new Text("How to Play:");
-		tutorialText.setFont(Font.font("Times New Roman", FontWeight.BOLD, 30));
+		tutorialText.setFont(Font.font("Times New Roman", FontWeight.BOLD, 20));
 		tutorialText.setFill (Color.BLACK);
-		grid.add(tutorialText, 0, 0);
+		grid.add(tutorialText, 3, 5);
 		
-		stack.getChildren().addAll(button2, tutorialText);
+		Text conveyorsInstructions = new Text("Click on '<','^', or '>' to change the conveyors' direction.");
+		conveyorsInstructions.setFont(Font.font("Monospaced", FontWeight.BOLD, 15));
+		conveyorsInstructions.setFill (Color.BLACK);
+		grid.add(conveyorsInstructions, 3, 10);
 		
-        Group view2 = new Group();
-        view2.getChildren().addAll(tutorialText, button2);
-
+		Text go = new Text("Click 'go!' to start the game. The shapes will start moving on the conveyor.");
+		go.setFont(Font.font("Monospaced", FontWeight.BOLD, 15));
+		go.setFill (Color.BLACK);
+		grid.add(go, 3, 11);
+		
+		Text restart = new Text("Click 'restart' to reset the game.");
+		restart.setFont(Font.font("Monospaced", FontWeight.BOLD, 15));
+		restart.setFill (Color.BLACK);
+		grid.add(restart, 3, 15);
+		
+		Text close = new Text("Click 'close' to quit the game.");
+		close.setFont(Font.font("Monospaced", FontWeight.BOLD, 15));
+		close.setFill (Color.BLACK);
+		grid.add(close, 3, 15);
+		
+		Button button2 = new Button("Click this box to play the game!");
+        button2.setOnAction(e -> window.setScene(scene1));
+        grid.add(button2, 0, 0);
+		
+		GridPane grid2 = new GridPane();
+		grid2.getChildren().addAll(tutorialText, conveyorsInstructions, restart, go, button2);
+		
         StackPane root = new StackPane(view1);
-
-        // Slide show
-		nextView.setOnAction(event -> {
-			root.getChildren().add(view2);
+		nextView.setOnAction(event -> { 
+			root.getChildren().add(grid2);
 			double width = root.getWidth();
-			KeyFrame start = new KeyFrame(Duration.ZERO, new KeyValue(view2.translateXProperty(), width),
+			KeyFrame start = new KeyFrame(Duration.ZERO, new KeyValue(grid2.translateXProperty(), width),
 					new KeyValue(view1.translateXProperty(), 0));
-			KeyFrame end = new KeyFrame(Duration.seconds(1), new KeyValue(view2.translateXProperty(), 0),
+			KeyFrame end = new KeyFrame(Duration.seconds(1), new KeyValue(grid2.translateXProperty(), 0),
 					new KeyValue(view1.translateXProperty(), -width));
 			Timeline slide = new Timeline(start, end);
 			slide.setOnFinished(e -> root.getChildren().remove(view1));
@@ -131,12 +151,11 @@ public class SlidingViewTest extends Application {
 		});
 		
         Scene finalScene = new Scene(root, WIDTH, HEIGHT);
-        
     	////////////////////SCENE 3: Play "Main" class/////////////////////////
         
-		scene3 = ((Main.class));
+		//scene3 = ((Main.class));
+		
 		// Show scene    
         primaryStage.setScene(finalScene);
         primaryStage.show();
     }
-}
