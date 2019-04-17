375e6c6189d06d036b6af872ee2201032025b7fb
Juan Callejas <jcallejasrodriguez@gmail.com>
2019-04-17 11:31:24 -0400

    Update Shape.java
    +Defined static final string to store path to directory containing payload sprites
    +Moved all sprite paths to static final array of strings
    +Optimized main switch statement in Shape constructor

--- src/Game/Shape.java
+++ src/Game/Shape.java
@@ -7,48 +7,50 @@ enum ShapeTypes {
 }
 
 public class Shape extends GridTileBase {
+	
+	//Path where all payload sprites are located
+	private static final String imgPath_Dir = "../Res/ConveyorPayloads/";
+	//Paths for Circle sprites. Stored in array for easy randomization
+    private static final String[] imgPath_Circle = new String[] {
+    		imgPath_Dir + "Payload.Circle_8Ball.png",
+			imgPath_Dir + "Payload.Circle_Donut.png",
+			imgPath_Dir + "Payload.Circle_ChocoDonut.png"
+	};
+	//Paths for Square sprites. Stored in array for easy randomization
+	private static final String[] imgPath_Square = new String[] {
+			imgPath_Dir + "Payload.Square_Crate.png",
+			imgPath_Dir + "Payload.Square_Parcel.png",
+			imgPath_Dir + "Payload.Square_ParcelTilted.png"
+	};
+	//Paths for Triangle sprites. Stored in array for easy randomization
+	private static final String[] imgPath_Triangle = new String[] {
+			imgPath_Dir + "Payload.Triangle_Cheese.png",
+			imgPath_Dir + "Payload.Triangle_Watermelon.png"
+	};
 
     private ShapeTypes shape;
     
     public Shape(ShapeTypes shape) {
         
         this.shape = shape;
+        
+        float rand = (float)Math.random();
+        String[] paths;
     
         switch (shape) {
-        
-            case circle: {
-            
-                switch ((int)(Math.random()*3)+1) {
-                    case 1:{ setAnimPath("../Res/Payload.Circle_8Ball.png");break;}
-                    case 2:{ setAnimPath("../Res/Payload.Circle_ChocoDonut.png");break;}
-                    case 3:{ setAnimPath("../Res/Payload.Circle_Donut.png");break;}
-                }
-            
+            case circle:
+				paths = imgPath_Circle;
                 break;
-            }
-            
-            case square: {
-            
-                switch ((int)(Math.random()*3)+1) {
-                    case 1:{ setAnimPath("../Res/Payload.Square_Crate.png");break;}
-                    case 2:{ setAnimPath("../Res/Payload.Square_Parcel.png");break;}
-                    case 3:{ setAnimPath("../Res/Payload.Square_ParcelTilted.png");break;}
-                }
-            
+            case square:
+				paths = imgPath_Square;
                 break;
-            }
-            
-            case triangle: {
-            
-                switch ((int)(Math.random()*2)+1) {
-                    case 1:{ setAnimPath("../Res/Payload.Triangle_Cheese.png");break;}
-                    case 2:{ setAnimPath("../Res/Payload.Triangle_Watermelon.png");break;}
-                }
-            
+			default://Set paths var to triangle by default to avoid undefinedArray exceptions
+            case triangle:
+            	paths = imgPath_Triangle;
                 break;
-            }
-            
         }
+        
+        setAnimPath(paths[Math.round(rand * paths.length)]);
     
     }
     
